6d115f4b5f25bb0d27707ac6cabb97faeaf6b9b4
ismagaia <matheusduartec96@gmail.com>
2021-05-07 09:50:30 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
+import Footer from "componentes/Footer";
+import NavBar from "componentes/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+    <>
+      <NavBar />
       <div className="container">
-        <nav className="my-2 my-md-0 mr-md-3"></nav>
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Acessar dashboard
+          </Link>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
